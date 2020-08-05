f32e139cd9d3a9aba5d1cd79dec2592ea8ed3f17
rexsteroxy <rexsteroxy22@gmail.com>
2020-08-05 23:56:55 +0100

    migration

--- src/Migrations/Version20200738948357.php
+++ src/Migrations/Version20200738948357.php
@@ -22,8 +22,8 @@ final class Version20200728001402 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('CREATE TABLE movie (id INT AUTO_INCREMENT NOT NULL, title VARCHAR(255) NOT NULL, name VARCHAR(255) NOT NULL,
-         description VARCHAR(255) NOT NULL, type VARCHAR(255) NOT NULL,
+        $this->addSql('CREATE TABLE movie (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL,
+         description VARCHAR(255) NOT NULL,
         
          PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
     }
